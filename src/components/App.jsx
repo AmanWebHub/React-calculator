22c92467fec6d698699ccd70b6698e1097ee98e1
Aman <zbat37@gmail.com>
2025-05-30 17:49:32 +0530

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Display from './Display'
+
+const App = () => {
+
+  return (
+    <div className='container'>
+      <h1 id="header">React Calculator</h1>
+      <div className="calc-body">
+        <Display />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Display = () => {
+    return (
+        <div className="display">
+            <input type="text" className="display-top" disabled />
+            <div className="display-bottom"></div>
+        </div>
+    )
+}
+
+export default Display
